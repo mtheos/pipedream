1611189015fe8318661c00ec59b355e9d4ec1206
Michael Theos <michael@theos.me>
2022-09-03 20:33:41 +1000

    Local maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("maven-publish")
 }
 
 group = "me.theos"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
@@ -22,4 +27,24 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "pipedream"
+            from(components["kotlin"])
+            pom {
+                packaging = "jar"
+                name.set("pipedream")
+                description.set("A library for creating pipes that tee")
+                developers {
+                    developer {
+                        id.set("mtheos")
+                        name.set("Michael Theos")
+                    }
+                }
+            }
+        }
+    }
 }
